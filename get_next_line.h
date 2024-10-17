bfd2afb3440755376499a58b72feea9e061ac39f
romashko <romashko@duck>
2024-10-17 12:33:06 +0200

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romashko <romashko@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 08:40:11 by romashko          #+#    #+#             */
+/*   Updated: 2024/10/09 08:47:31 by romashko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
